dab550a23b9533d4d5e213dfedfd35031965d59c
ErikboundStudios <60669742+Erikbound@users.noreply.github.com>
2024-10-28 15:09:49 -0400

    test

--- frontend/src/pages/About.js
+++ frontend/src/pages/About.js
@@ -7,6 +7,7 @@ const About = ( {onCreatePost}) => {
         <Navbar onCreatePost={onCreatePost}/>
         <div className='pageText'>
             <h2>About Page</h2>
+            <p>5Chan is an amazing and 100% original forum where anyone can post whatever useless nonsense you like!</p>
         </div>
     </div>
   )
