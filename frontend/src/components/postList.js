8578ab1fa70df255245c5ba5e9d00b12390670da
Michael Reifer <Mjreifer1@gmail.com>
2024-10-29 09:32:43 -0400

    fixed post list null id

--- frontend/src/components/postList.js
+++ frontend/src/components/postList.js
@@ -2,16 +2,16 @@ import React from 'react';
 
 function PostList({ posts }) {
   return (
-    <div class="postList">
+    <div className="postList">
       <h2 style={{color: 'white'}}>Posts:</h2>
         {posts.map((post) => (
-          <li key={post.id} className='postItem'>
+          <div key={post._id} className='postItem'>
             <div className ="inListPost">
             <h3 className='inPostTitle'>{post.title}</h3>
             <p className='inPostauthor'>Author: {post.author}</p>
             <p className='inPostText'>{post.text}</p>
             </div>
-          </li>
+          </div>
         ))}
     </div>
   );
